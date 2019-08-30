c64b9664c10ab53e67811044cb26e0724ceff4d4
Leonardo Bianchini <sbwnco@gmail.com>
2019-08-30 20:11:36 +0200

    Manca da hgighlightare o bordi di una tile quando ci passi copra il con iol,ouse

---
+++ Unity-2D-RPGEngine/Assets/tileMapAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class tileMapAgent : MonoBehaviour
+{
+    public Tilemap Tilemap;
+    //TileBase
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseinput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        int x = Mathf.FloorToInt(mouseinput.x);
+        int y = Mathf.FloorToInt(mouseinput.y);
+        int z = Mathf.FloorToInt(mouseinput.z);
+
+        var focustile = Tilemap.GetTile(new Vector3Int(x, y, z));
+
+        //focustile. Rimane da focusare la tile quando ci focussi sopra con iol mouse
+    }
+}
