75fd58bac8d991d5ec0c6876287c2d16ef0cfc61
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2019-09-03 08:14:05 +0200

    Ho aggiunto una nuova scena dove inizializzare una nuova griglia con GameObject a scelta tra green, land, water, ecc; manca l'inizializzazione degli oggetti

---
+++ Assets/GridSystem/GridBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviour : MonoBehaviour
+{
+    public int Rows = 10;
+    public int Columns = 10;
+    public int Scale = 1;
+    public GameObject GridPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                GameObject obj = Instantiate(GridPrefab, ); // rimasto al riempimento della griglia con gli oggetti
+            }
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawSphere(new Vector3(0,0,0), 1);
+    //}
+}
