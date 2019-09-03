7cacf27c2268562325fff9667846d39c112ab508
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2019-09-03 17:53:36 +0200

    Aggiunto gizmo sul cnetro griglia; adesso inizializa la griglia con uno sprite dal disegno di erba

--- Assets/GridSystem/GridBehaviour.cs
+++ Assets/GridSystem/GridBehaviour.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GridBehaviour : MonoBehaviour
 {
     public int Rows = 10;
     public int Columns = 10;
     public int Scale = 1;
-    public GameObject GridPrefab;
+    public Vector2Int StartingGrid;
+    public GameObject GrassTile;
+    public GameObject EarthTile;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void GenerateGrid()
     {
-        for (int i = 0; i < Rows; i++)
+        for (int i = -Rows/2; i < Rows/2; i++)
         {
-            for (int j = 0; j < Columns; j++)
+            for (int j = -Columns/2; j < Columns/2; j++)
             {
-                GameObject obj = Instantiate(GridPrefab, ); // rimasto al riempimento della griglia con gli oggetti
+                GameObject obj = Instantiate(GrassTile, new Vector3(0.0f + i * Scale, 0.0f + j * Scale, 0.0f), new Quaternion()); // rimasto al riempimento della griglia con gli oggetti
             }
         }
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawSphere(new Vector3(0,0,0), 1);
-    //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(new Vector3(StartingGrid.x, StartingGrid.y, 0), 0.1f);
+    }
 }
