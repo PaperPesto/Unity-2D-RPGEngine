a47fe23fd707635b9cedf2c3a0369cad15be2c71
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2019-09-05 19:07:11 +0200

    Aggiunta status bar un po alla cazzo; rimane centrata e non si muove come dovrebbe: come funziona il localposition?

---
+++ Assets/HealthBar/HealthComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public int ActualHealth = 10;
+    public int MaxHealth = 10;
+
+    public int xe;
+    public int ye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scale = (float)ActualHealth / (float)MaxHealth;
+        transform.GetChild(0).transform.localScale = new Vector2(10 * scale, 1);
+        transform.GetChild(0).transform.localPosition = new Vector2(0, -8);
+    }
+}
